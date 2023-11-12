f3cd7a09386a272127299e8cacf9d132390c3c8d
nav302004 <navv5803@gmail.com>
2023-11-12 22:42:49 +0530

    Header responsive complete

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import "./Header.css"
 import { Detector } from "react-detect-offline";
 import { BsBatteryCharging, BsBatteryFull, BsWifi, BsWifiOff, BsSoundwave } from "react-icons/bs";
+import { PiNotification, PiCalendarBlankLight } from "react-icons/pi"
 import Modaldate from './Modaldate';
 import Modalnotification from './Modalnotification';
 import Modalsetting from './Modalsetting';
@@ -10,10 +11,10 @@ import { useBattery } from 'react-use';
 const Header = () => {
     const battery = useBattery();
     const { isSupported, level, charging, dischargingTime, chargingTime } = battery;
-    
-    const [isnotification,setnotification] = useState(false)
-    const [isdate,setdate] = useState(false)
-    const [issetting,setsetting] = useState(false)
+
+    const [isnotification, setnotification] = useState(false)
+    const [isdate, setdate] = useState(false)
+    const [issetting, setsetting] = useState(false)
 
     const notificationHandler = () => {
         setnotification(!isnotification);
@@ -33,10 +34,14 @@ const Header = () => {
 
     return (
         <React.Fragment>
-            <div className="Modal-display">{isnotification && <Modalnotification/>}</div>
-            <div className="Modal-display">{isdate && <Modaldate/>} </div>
-            <div className="Modal-display">{issetting && <Modalsetting/>}</div>
+            <div className="Modal-display">{isnotification && <Modalnotification />}</div>
+            <div className="Modal-display">{isdate && <Modaldate />} </div>
+            <div className="Modal-display">{issetting && <Modalsetting />}</div>
             <div className="Header">
+                <div className="Header-responsive">
+                    <div className="Header-notification-responsive" onClick={notificationHandler} > <PiCalendarBlankLight /> </div>
+                    <div className="Header-date-responsive" onClick={dateHandler} ><PiNotification /></div>
+                </div>
                 <div className="Header-notification" onClick={notificationHandler} ><p>Notification(1)</p></div>
                 <div className="Header-date" onClick={dateHandler} ><p>October 15 2023</p></div>
                 <div className="Header-setting" onClick={settingHandler} >
